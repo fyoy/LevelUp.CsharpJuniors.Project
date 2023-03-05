981c3d17987fee869ab7c57ab707fa4cc8c462a2
fyoy <fedorzavaley@gmail.com>
2023-03-05 15:27:48 +0300

    Только для значка YOLO

--- FedorStore.Api/Program.cs
+++ FedorStore.Api/Program.cs
@@ -2,18 +2,13 @@ using FedorStore.Api.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-      
 builder.Services.AddScoped<IProductService , ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,9 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
